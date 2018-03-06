713ea71ca5947f94c24a4ccb126f45e17c668ebf
pcurich <pcurich@digitalmeasures.com>
2018-03-06 16:42:02 -0600

    Updating build dependency paths.

--- src/main/java/guru/qas/martini/jmeter/processor/MartiniPreProcessor.java
+++ src/main/java/guru/qas/martini/jmeter/processor/MartiniPreProcessor.java
@@ -16,11 +16,20 @@ limitations under the License.
 
 package guru.qas.martini.jmeter.processor;
 
+import java.util.List;
+
 import org.apache.jmeter.processor.PreProcessor;
 import org.apache.jmeter.testelement.AbstractTestElement;
 import org.apache.jmeter.testelement.TestStateListener;
+import org.apache.jmeter.testelement.property.JMeterProperty;
+import org.apache.jmeter.testelement.property.ObjectProperty;
+import org.apache.jmeter.threads.AbstractThreadGroup;
+import org.apache.jmeter.threads.JMeterContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.google.common.base.Splitter;
 
 public final class MartiniPreProcessor extends AbstractTestElement implements PreProcessor, TestStateListener {
 
@@ -38,14 +47,39 @@ public final class MartiniPreProcessor extends AbstractTestElement implements Pr
 
 	public void testStarted() {
 		LOGGER.debug("in testStarted()");
+
+		String joined = getConfigLocations();
+		List<String> locations = Splitter.on(',').omitEmptyStrings().trimResults().splitToList(joined);
+		String[] locationArray = locations.toArray(new String[locations.size()]);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(locationArray);
+		AbstractThreadGroup threadGroup = getThreadGroup();
+		ObjectProperty property = new ObjectProperty(PROPERTY_SPRING_CONTEXT, context);
+		threadGroup.setTemporary(property);
+	}
+
+	private AbstractThreadGroup getThreadGroup() {
+		JMeterContext threadContext = getThreadContext();
+		return threadContext.getThreadGroup();
 	}
 
 	public void testStarted(String host) {
 		LOGGER.debug("in testStarted({})", host);
 	}
 
+	private static final String PROPERTY_SPRING_CONTEXT = "martini.spring.context";
+
 	public void testEnded() {
 		LOGGER.debug("in testEnded()");
+		AbstractThreadGroup threadGroup = getThreadGroup();
+		JMeterProperty property = threadGroup.getProperty(PROPERTY_SPRING_CONTEXT);
+		if (null != property) {
+			threadGroup.removeProperty(PROPERTY_SPRING_CONTEXT);
+			Object o = property.getObjectValue();
+			if (ClassPathXmlApplicationContext.class.isInstance(o)) {
+				ClassPathXmlApplicationContext applicationContext = ClassPathXmlApplicationContext.class.cast(o);
+				applicationContext.close();
+			}
+		}
 	}
 
 	public void testEnded(String host) {
