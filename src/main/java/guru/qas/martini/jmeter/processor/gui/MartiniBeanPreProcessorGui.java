0abea33768f1f978e112c8aa2b55aab881950fc6
pcurich <pcurich@digitalmeasures.com>
2018-03-19 11:07:42 -0500

    Removed unused constant.

--- src/main/java/guru/qas/martini/jmeter/processor/gui/MartiniBeanPreProcessorGui.java
+++ src/main/java/guru/qas/martini/jmeter/processor/gui/MartiniBeanPreProcessorGui.java
@@ -35,7 +35,6 @@ public final class MartiniBeanPreProcessorGui extends AbstractPreProcessorGui {
 
 	private static final long serialVersionUID = 4447406345771389792L;
 
-	protected static final String INSTRUCTIONS = "blah blah blah";
 	protected static final String DEFAULT_NAME = "aBean";
 	protected static final String DEFAULT_TYPE = "com.mycompany.MyPreProcessorBean";
 
