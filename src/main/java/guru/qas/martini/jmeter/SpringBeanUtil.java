3b5aa6017b814ccdcae64759e127c98f244b1375
pcurich <pcurich@digitalmeasures.com>
2018-06-06 13:06:44 -0500

    Added debugging statements to diagnose an issue with JMeter runs of SpringBeanUtil in slave configuration.

--- src/main/java/guru/qas/martini/jmeter/SpringBeanUtil.java
+++ src/main/java/guru/qas/martini/jmeter/SpringBeanUtil.java
@@ -18,6 +18,8 @@ package guru.qas.martini.jmeter;
 
 import javax.annotation.Nonnull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 
 import guru.qas.martini.jmeter.processor.MartiniSpringPreProcessor;
@@ -27,28 +29,39 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @SuppressWarnings("WeakerAccess")
 public class SpringBeanUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringBeanUtil.class);
+
 	public static <T> T getBean(String name, String type, @Nonnull Class<T> expectedType) {
 		checkNotNull(expectedType, "null Class");
+		LOGGER.debug("getBean({}, {}, {})", name, type, expectedType);
 
 		String normalizedName = getNormalized(name);
+		LOGGER.debug("normalized name is {}", name);
+
 		String normalizedType = getNormalized(type);
+		LOGGER.debug("normalized type is {}", type);
 
 		ApplicationContext context = MartiniSpringPreProcessor.getApplicationContext();
 		Class<? extends T> implementation = null == normalizedType ? null : getImplementation(context, normalizedType);
+		LOGGER.debug("implementation is {}", implementation);
 
 		Object bean;
 		if (null == normalizedName && null == implementation) {
 			throw new RuntimeException("at least one of bean name or bean type must be specified");
 		}
 		else if (null == normalizedName) {
+			LOGGER.debug("fetching bean by implementation: {}", implementation);
 			bean = context.getBean(implementation);
 		}
 		else if (null == implementation) {
+			LOGGER.debug("fetching bean by name: {}", normalizedName);
 			bean = context.getBean(normalizedName);
 		}
 		else {
+			LOGGER.debug("fetching bean by name {} and implementation {}", normalizedName, implementation);
 			bean = context.getBean(normalizedName, implementation);
 		}
+		LOGGER.debug("retrieved bean {}", bean);
 		return expectedType.cast(bean);
 	}
 
@@ -64,9 +77,11 @@ public class SpringBeanUtil {
 	protected static Class getImplementation(ApplicationContext springContext, String type) {
 		ClassLoader loader = springContext.getClassLoader();
 		try {
+			LOGGER.debug("loading class of type {} with ClassLoader {}", type, loader);
 			return Class.forName(type, true, loader);
 		}
 		catch (ClassNotFoundException e) {
+			LOGGER.error("unable to load class {}", type, e);
 			throw new RuntimeException(e);
 		}
 	}
